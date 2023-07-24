abb5e2d038d481f8507bb74c66f0e46fb14f1bfc
khushi-jain-17 <khushisjain33@gmail.com@domain.com>
2023-07-24 11:58:21 +0530

    day8

--- db/schema.rb
+++ db/schema.rb
@@ -80,7 +80,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_19_101928) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.integer "admin"
-    t.integer "role", default: 0
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
